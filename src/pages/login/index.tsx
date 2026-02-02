a298b97fd61dd57f5c0929906c67342e27a8761c
TTOCHIwas <95687307+TTOCHIwas@users.noreply.github.com>
2026-02-02 16:13:20 +0900

    feat: 역할별 레이아웃, lazy loading 적용 라우터 구성

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
+
+const LoginPage = () => {
+  return (
+    <Box p={10} textAlign="center">
+      <Heading mb={4}>로그인 페이지</Heading>
+    </Box>
+  );
+};
+
+export default LoginPage;
