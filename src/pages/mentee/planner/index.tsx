2ab25972491bac54d76c86e628971a4a98074034
TTOCHIwas <95687307+TTOCHIwas@users.noreply.github.com>
2026-02-02 19:54:09 +0900

    feat: 플래너 페이지 구현 + chore: 파일명 오타 수정- TackItem.tsx -> TaskItem.tsx

--- src/pages/mentee/planner/index.tsx
+++ src/pages/mentee/planner/index.tsx
@@ -1,10 +1,24 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Container, Divider } from '@chakra-ui/react';
+import { PlannerHeader } from '@/widgets/planner/PlannerHeader';
+import { TaskList } from '@/widgets/planner/TaskList';
+// import { StudyTimeWidget } from '@/widgets/planner/StudyTimeWidget';
+import { LogoutButton } from '@/features/auth';
 
 const MenteePlannerPage = () => {
   return (
-    <Box p={5}>
-      <Heading size="lg" mb={4}>일일 플래너 (멘티 메인)</Heading>
-    </Box>
+    <Container maxW="md" p={0} minH="100vh" bg="gray.50">
+      <PlannerHeader />
+      
+      <Box px={4} py={2}>
+        <TaskList />
+        
+        <Divider my={6} />
+        
+        <Box textAlign="center" py={4}>
+          <LogoutButton />
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
